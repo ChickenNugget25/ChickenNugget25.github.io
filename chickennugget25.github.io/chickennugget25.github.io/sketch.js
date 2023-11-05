a760a9ce0f881f27a6a5ae0b7e4e21cfb55fb639
Parker Borek <81893842+ChickenNugget25@users.noreply.github.com>
2023-11-05 17:42:14 -0500

    re2

--- chickennugget25.github.io/chickennugget25.github.io/sketch.js
+++ chickennugget25.github.io/chickennugget25.github.io/sketch.js
@@ -1,17 +1,16 @@
-let modelObject;
+let model;
 
 function preload() {
-  modelObject = loadModel('cube.obj');
+  model = loadModel('cube.obj');
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight,WEBGL);
-  console.log("here");
 }
 
 function draw() {
 	background(0);
   scale(0.05);
   rotateX(frameCount * 0.01);
-  model(modelObject);
+  model(model);
 }
---
+++ chickennugget25.github.io/sketch.js
@@ -0,0 +1,16 @@
+let model;
+
+function preload() {
+  model = loadModel('cube.obj');
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight,WebGL);
+}
+
+function draw() {
+	background(0);
+  scale(0.05);
+  rotateX(frameCount * 0.01);
+  model(model);
+}
